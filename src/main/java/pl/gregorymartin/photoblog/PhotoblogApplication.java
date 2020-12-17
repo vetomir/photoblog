03bfe2d637c4abca492617b7d2d8941c68d09d31
Grzegorz Marcin Szczerba <vetomir@gmail.com>
2020-12-17 23:02:46 +0100

    Initial commit

---
+++ src/main/java/pl/gregorymartin/photoblog/PhotoblogApplication.java
@@ -0,0 +1,13 @@
+package pl.gregorymartin.photoblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoblogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/gregorymartin/photoblog/PhotoblogApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.gregorymartin.photoblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
